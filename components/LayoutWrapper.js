7d7c832ed6e58a8f892b736fdf19a5e8cbed6b85
mshrynzw <iq87io25@gmail.com>
2023-04-08 22:47:57 +0900

    Edit logo

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import Image from '@/components/Image'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -16,7 +16,13 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  <Image
+                    alt="ocean"
+                    src="/static/images/logo.png"
+                    width={43.61}
+                    height={43.61}
+                    className="shadow-md"
+                  />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
