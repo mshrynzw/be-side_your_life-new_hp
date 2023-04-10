75f4f780a8562651022475f6faefe6a8366d5db1
mshrynzw <iq87io25@gmail.com>
2023-04-10 23:58:05 +0900

    Add button

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 
 import NewsletterForm from '@/components/NewsletterForm'
+import PostIcon from '@/components/post-icons'
 
 const MAX_DISPLAY = 3
 
@@ -32,6 +33,12 @@ export default function Home({ posts }) {
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
             const { slug, date, title, summary, tags } = frontMatter
+            const hash_title =
+              'vol' + title.replace('石川・ホンマ・ぶるんのBe-SIDE Your Life! vol.', '')
+            const search_href =
+              'https://twitter.com/hashtag/%E3%83%93%E3%83%BC%E3%82%B5%E3%82%A4?src=hashtag_click&f=live'
+            const post_href = `https://twitter.com/intent/tweet?hashtags=ビーサイ,${hash_title}`
+
             return (
               <li key={slug} className="pb-4 pt-2">
                 <article>
@@ -59,11 +66,9 @@ export default function Home({ posts }) {
                             ))}
                           </div>
                         </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
-                        </div>
+                        <div className="max-w-none text-gray-500 dark:text-gray-400">{summary}</div>
                       </div>
-                      <div className="text-center text-base font-medium leading-6">
+                      <div className="text-base font-medium leading-6">
                         <Link
                           href={`/blog/${slug}`}
                           className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
@@ -71,6 +76,22 @@ export default function Home({ posts }) {
                         >
                           Read more...
                         </Link>
+                      </div>
+                      <div className="pb-6 pt-4 text-sm text-gray-700 dark:text-gray-300">
+                        <div className="flex flex-row justify-center space-x-8">
+                          <Link href={search_href} rel="nofollow">
+                            <div className="flex w-32 flex-row items-center justify-around rounded-md bg-blue-500 px-6 py-1 text-white shadow-md shadow-blue-500/50 hover:bg-blue-500/80">
+                              <PostIcon kind="twitter" size="6" />
+                              <div className="font-extrabold">Search</div>
+                            </div>
+                          </Link>
+                          <Link href={post_href} rel="nofollow">
+                            <div className="flex w-32 flex-row items-center justify-around rounded-md bg-blue-500 px-6 py-1 text-white shadow-md shadow-blue-500/50 hover:bg-blue-500/80">
+                              <PostIcon kind="twitter" size="6" />
+                              <div className="font-extrabold">Post</div>
+                            </div>
+                          </Link>
+                        </div>
                       </div>
                     </div>
                   </div>
