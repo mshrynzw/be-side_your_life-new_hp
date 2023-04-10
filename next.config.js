7200a6af1b2aa59b10d9fdd38e46bbffef70b757
mshrynzw <iq87io25@gmail.com>
2023-04-11 00:43:35 +0900

    Fix media-src

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const ContentSecurityPolicy = `
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
-  media-src "'self'", 'data:', 'blob:'];
+  media-src ["'self'", 'data:', 'blob:', 'project-phi.ddo.jp'];
   connect-src *;
   font-src 'self';
   frame-src giscus.app
