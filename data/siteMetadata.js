801850176a2793952bb07c8ac94b84bef701c0de
mshrynzw <iq87io25@gmail.com>
2023-04-11 12:01:30 +0900

    Edit locale

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -14,8 +14,7 @@ const siteMetadata = {
   email: 'biho@be-side.jp',
   twitter: 'https://twitter.com/be_side_jp',
   youtube: 'https://www.youtube.com/user/besidetv',
-  locale: 'en-us',
-  // TODO EDIT
+  locale: 'ja',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
