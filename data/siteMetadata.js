76f4327e54412dbfe45d9cb8db09066d6b4f41a6
kaoruyuki <>
2024-04-12 09:44:46 +0900

    Change: Domain

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   `,
   language: 'ja',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://be-side-your-life.com',
+  siteUrl: 'https://be-side-your.life',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/be-side.jpg',
@@ -20,7 +20,7 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: 'be-side-your-life.com', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: 'be-side-your.life', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: 'G-H6ZJ9HT99R', // e.g. UA-000000-2 or G-XXXXXXX
