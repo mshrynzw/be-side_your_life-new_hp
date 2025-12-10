b5bb3cc62d7bf980dedcb43ca591ee1c8d624709
mshrynzw <iq87io25@gmail.com>
2025-12-11 03:58:45 +0900

    Refactor: update layout and text color in Home component

--- pages/index.js
+++ pages/index.js
@@ -18,14 +18,14 @@ export async function getStaticProps() {
 
 export default function Home({ posts }) {
   return (
-    <>
+    <div className="bg-background-blur mt-6 rounded-lg bg-gray-900/20 px-24 dark:bg-gray-900/70">
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-[#AE0B11] sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          <p className="text-lg leading-7 text-gray-100 dark:text-gray-400">
             {siteMetadata.description}
           </p>
         </div>
@@ -117,6 +117,6 @@ export default function Home({ posts }) {
           <NewsletterForm />
         </div>
       )}
-    </>
+    </div>
   )
 }
